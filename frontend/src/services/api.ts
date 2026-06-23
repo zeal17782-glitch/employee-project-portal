fdb564116e9a4009f7f09b1f4a5ee5209da2eb63
https://github.com/zeal17782-glitch <zeal.17782@sakec.ac.in>
2026-06-24 04:46:38 +0530

    Update API URL to production backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-
+const API_URL = 'https://employee-project-portal-production.up.railway.app/api';
 const api = axios.create({
   baseURL: API_URL,
   headers: {
